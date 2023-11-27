546553887f10e74964c5281c53063337f64c621c
Víctor Henrique <103254170+ViktorHenrique@users.noreply.github.com>
2023-11-27 02:03:02 -0300

    Add files via upload

---
+++ Engenharia de Software III /Singleton/Code/Time.java
@@ -0,0 +1,15 @@
+public class Time {
+    private static Time time;
+    private Time() {}
+
+    public static Time getInstance() {
+        if (time == null) {
+            time = new Time();
+        }
+        return time;
+    }
+    
+    public void acompanhar(String evento) {
+        System.out.println("Acompanhando: " + evento);
+    }
+}
